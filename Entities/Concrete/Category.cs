22723e6a8014d03668440fa1f07e55dddd1db18b
sipan <sipan@MY-BOOK>
2021-01-31 00:05:53 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class Kalmasın
+    class Category:IEntity
+    {
+        public int CstegoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
